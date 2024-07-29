41609c5357a041d194fa35aaf4910a762e743934
Neeru Pareek <neerupareek18@gmail.com>
2024-07-29 19:50:43 +0530

    Basics about OOPs

---
+++ src/LiveClassPrograms_28072024/Lab001.java
@@ -0,0 +1,43 @@
+package LiveClassPrograms_28072024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        //Procedural oriented Programming -- only functions were created to do the tasks
+        //They were not maintained properly
+        //Doesn't much resemble with the real world
+        //Just following the procedures like robots
+
+        //Object-Oriented Programming -- more close to the real life
+        //OOPs is not a programming lang. but set of concepts, certain rules
+
+        /*
+        Creating a new Planet with AHumans --
+        Attributes --> name, age, eye color, no. of legs, no. of hands
+        Behaviour --> walk, eat, talk, rad, play, watch
+
+        Class --> combination of A&B, it is a blueprint to create real objects, entity with these A&B
+        A --> Properties
+        B --> methods
+            Whenever is function is seperately within the class, they are known as methods
+
+        Whe the class is created -- no memory is allocated
+        When the object is created, real entity -- memory gets allocated
+
+        main function is not required in class blueprint
+        main is required when object is to be created
+
+        JVM -- Class Loaders(class gets loaded) and Heap Area (Objects are created)
+        and very small memory for SCP
+         */
+
+        /*
+        Animal a = null; -- no memory is created in this case, as the reference will point to null area
+         */
+
+        /*
+        Class Loader loads the class only one time -- no matter how many times you write
+
+         */
+        //Refer the notes for further description
+    }
+}
