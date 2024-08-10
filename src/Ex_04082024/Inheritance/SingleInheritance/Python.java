64a2aececd0139b01b13cea9f2e349944473761b
Neeru Pareek <neerupareek18@gmail.com>
2024-08-10 12:15:08 +0530

    Super Keyword

--- src/Ex_04082024/Inheritance/SingleInheritance/Python.java
+++ src/Ex_04082024/Inheritance/SingleInheritance/Python.java
@@ -2,7 +2,8 @@ package Ex_04082024.Inheritance.SingleInheritance;
 
 public class Python extends Programming{
     Python(){
-        super("Version2"); //without this, error is coming
-        System.out.println("Python 'IS-A' type of programming");
+        super("Version2");
+        //If the child class has DC, Parent should also have DC or call the PC of Parent
+//        System.out.println("Python 'IS-A' type of programming");
     }
 }
